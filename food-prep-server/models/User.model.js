be9e340004e723689af64d6ed96ff89ebb2be374
Shawn Wolter <woltersh@outlook.de>
2023-05-09 17:44:58 +0200

    add user model

--- food-prep-server/models/User.model.js
+++ food-prep-server/models/User.model.js
@@ -13,6 +13,55 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, 'Password is required.']
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required."],
+      trim: true
+    },
+    gender: {
+      type: String,
+      required: [true, "Gender is required."],
+      enum: ["Male", "Female"]
+    },
+    age: {
+      type: Number,
+      required: [true, "Age is required."]
+    },
+    size: {
+      type: Number,
+      required: [true, "Size is required."]
+    },
+    weight: {
+      type: Number,
+      required: [true, "Weight is required."]
+    },
+    goal: {
+      type: String,
+      required: [true, "Goal is required."],
+      enum: ["Lose Weight", "Gain Weight", "Keep Weight"]
+    },
+    bmi: {
+      type: Number
+    },
+    calorieDemand: {
+      type: Number
+    },
+    preferences: {
+      type: String,
+      required: [true, "Preferences are required."],
+      enum: ["No Preference", "Vegetarian", "Vegan", "Low Carb"]
+    },
+    time: {
+      type: String,
+      required: [true, "Time is required."],
+      enum: ["15 min", "30 min", "60 min"]
+    },
+    excludedIngredients: {
+      type: Array
+    },
+    fridge: {
+      type: Array
     }
   },
   {
