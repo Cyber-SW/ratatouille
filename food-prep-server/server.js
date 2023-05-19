49734bd28483cf5f028d5b4147ab339d49bd3468
Shawn Wolter <woltersh@outlook.de>
2023-05-19 16:26:55 +0200

    http port

--- food-prep-server/server.js
+++ food-prep-server/server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 //server port
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0" ,() => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
